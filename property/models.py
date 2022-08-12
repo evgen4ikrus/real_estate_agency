b8cbe2b44ca8568c8a624c6e1c620dbcc700e743
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-12 09:11:03 +0300

    Высушил поля модели Owner

--- property/models.py
+++ property/models.py
@@ -90,12 +90,12 @@ class Owner(models.Model):
         max_length=200,
         db_index=True
     )
-    owners_phonenumber = models.CharField(
+    phonenumber = models.CharField(
         'Номер владельца',
         max_length=20,
         db_index=True
     )
-    owner_pure_phone = PhoneNumberField(blank=True, null=True, db_index=True)
+    pure_phone = PhoneNumberField(blank=True, null=True, db_index=True)
     flats = models.ManyToManyField(
         Flat,
         related_name='owners',
